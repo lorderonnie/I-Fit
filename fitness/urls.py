cfb120624eacf8b3a988c4544c8b94eb9f6c7529
lorderonnie <rontheking45@gmail.com>
2020-01-23 14:36:56 +0300

    added url links to my view functions

--- fitness/urls.py
+++ fitness/urls.py
@@ -11,8 +11,10 @@ urlpatterns = [
     url(r'^updateprofile/$',views.updateprofile,name='updateprofile'),    
     path('logout/',views.logout,name = 'logout'),
     path('workout/',views.workout,name="workout"),
-    path('healthfact/',views.healthfacts,name="healthfacts")
-    
+    path('healthfact/',views.healthfacts,name="healthfacts"),
+    url(r'^workplan/',views.workplan,name='workplan'),
+    path('landingpage/',views.landingpage,name = 'landingpage'),
+    path('delete/<int:id>',views.delete,name='delete'),
     
 ]
 if settings.DEBUG:
